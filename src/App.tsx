3317ccc831689c6f4daae5a18f411fca3f48cceb
eagle-head <edy.kohn@gmail.com>
2023-07-23 19:23:09 -0300

    Add status bar component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { StatusBar } from "react-native";
 
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
@@ -7,6 +8,7 @@ import { Router } from "@routes";
 export function App(): JSX.Element {
   return (
     <SafeAreaProvider>
+      <StatusBar barStyle="light-content" />
       <Router />
     </SafeAreaProvider>
   );
