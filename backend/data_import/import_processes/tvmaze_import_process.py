996907df07089f80226002002ed78eeee6564181
matheushcamilo <matheushcamilo1997@gmail.com>
2023-10-20 16:55:47 -0300

    Add timer for the data import process

--- backend/data_import/import_processes/tvmaze_import_process.py
+++ backend/data_import/import_processes/tvmaze_import_process.py
@@ -1,6 +1,7 @@
 from data_import.import_processes.tvmaze_data_import import TVMazeImport
 from data_import.models import TVMazeShow
 import logging
+from timeit import default_timer as timer
 
 logger = logging.getLogger(__name__)
 
@@ -10,7 +11,12 @@ class TVMazeImportProcess:
     def import_data():
         logger.info("Starting TVMaze import process")
 
+        start = timer()
+
         tv_show_import = TVMazeImport(TVMazeShow, use_pagination=True)
         tv_show_import.import_data()
 
+        end = timer()
+
         logger.info("TVMaze import process finished")
+        logger.info(f"Time elapsed: {end - start} seconds")
